e07ebc151f01502d0e25b09f9f126d1c5957bbef
umunadine <umutesinad3@gmail.com>
2020-12-08 13:44:44 +0200

    my account

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for,flash, redirect
+from flask import render_template, url_for,flash, redirect, request
 from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Post
-from flask_login import login_user,current_user,logout_user
+from flask_login import login_user,current_user,logout_user,login_required
 
 posts = [
     {
@@ -56,7 +56,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return  redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Unsuccessfully login. Please check email and passwords', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -65,6 +66,11 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('myaccount.html', title='Account')
 
            
 
